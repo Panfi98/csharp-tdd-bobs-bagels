e903af36ca28760e7adbb4e843d4547d8063b32a
panfi98 <danielpanf@gmail.com>
2024-08-14 21:36:18 +0200

    Failing test 1 done

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,15 +1,28 @@
-namespace csharp_tdd_bobs_bagels.tests;
 
-public class Tests
+using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+
+namespace csharp_tdd_bobs_bagels.tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [TestFixture]
+        public class BasketTests
+        {
+            [TestCase("cremeCheese")]
+            // Test for story 1
+            public void Test(string bagelName) {
+                //arrange 
+                Basket basket = new Basket();
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+
+                //act
+                int result;
+
+                //assert
+                Assert.That(basket.items, Does.Contain(bagelName));
+            }
+        }
     }
 }
